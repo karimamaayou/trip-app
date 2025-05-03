7c9806a11e9e56908a5dc232c4ceb6e7625c25a1
Karimo21 <karimamaayou20@gmail.com>
2025-05-03 13:36:10 +0100

    fixed structure

---
+++ frontend/lib/screens/splashes/Main_splash.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/splashes/OnboardingScreen1.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    // Après 3 secondes, naviguer vers OnboardingScreen
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF2EB320),
+      body: Center(
+        child: Text(
+          'NextTrip',
+          style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      ),
+    );
+  }
+}
